2c917f044aa7d36e3ecdf834d7b5d82184b68138
mocobt <mocobt@gmail.com>
2020-02-29 17:33:26 +0900

    local_pretrainedmodels

--- cnn_finetune/__init__.py
+++ cnn_finetune/__init__.py
@@ -1,6 +1,6 @@
 __version__ = '0.6.0'
 
 
-from cnn_finetune.contrib.torchvision import *
-from cnn_finetune.contrib.pretrainedmodels import *
-from cnn_finetune.base import make_model
+from .contrib.torchvision import *
+from .contrib.pretrainedmodels import *
+from .base import make_model
